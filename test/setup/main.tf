c50bb0261cb52d890a824d226bf7532b4fac4b1d
Martijn van Schaardenburg <martijnvs@google.com>
2025-10-09 17:31:30 +0000

    chore: Per-module isolation for integration tests

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -24,17 +24,25 @@ locals {
       "bigtable.googleapis.com"
     ]
   }
+  extra_services_for_tests = {}
+  per_module_test_services = {
+    for module, services in local.per_module_services :
+    module => setunion(services, lookup(local.extra_services_for_tests, module, []))
+  }
 }
 
 module "project" {
+  for_each = local.per_module_test_services
+
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 18.0"
 
-  name              = "ci-bigtable"
-  random_project_id = "true"
-  org_id            = var.org_id
-  folder_id         = var.folder_id
-  billing_account   = var.billing_account
+  name                     = "ci-bigtable"
+  random_project_id        = "true"
+  random_project_id_length = 8
+  org_id                   = var.org_id
+  folder_id                = var.folder_id
+  billing_account          = var.billing_account
 
-  activate_apis = tolist(toset(flatten(values(local.per_module_services))))
+  activate_apis = each.value
 }
